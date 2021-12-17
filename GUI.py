1aa600d825441979d76939a8230eeef9e5f53558
Coooder-X <1104143015@qq.com>
2021-12-18 00:49:31 +0800

    feat: 重构GUI，添加tab页，支持文件拖入，批量处理

--- GUI.py
+++ GUI.py
@@ -2,28 +2,49 @@
 
 from tkinter import *
 import tkinter
+from tkinter import ttk
 import tkinter.messagebox
 import processData
 import os
+import windnd
 
 
-def showinfo():
-    # 获取输入的内容
-    fileName = entry.get()
-    if fileName == '':
-        tkinter.messagebox.showinfo('提示', '请输入文件绝对路径')
-        return
-    print(entry.get())
-    if os.path.isfile(fileName):
-        if fileName[-4:] != 'json':
-            tkinter.messagebox.showinfo('提示', '该文件不是json文件，请输入json文件')
+def dragged_files(listBox):
+
+    def updateListBox(files):
+        for item in files:
+            item = item.decode('gbk')
+            listBox.insert(END, item)
+
+    return updateListBox
+
+
+def showinfo(listBox):
+    def show():
+        fileNameList = []
+        if listBox.size() == 0:
+            tkinter.messagebox.showinfo('提示', '请拖入文件')
+            return
+        for i in range(listBox.size()):
+            fileName = listBox.get(i)
+            if fileName == '':
+                tkinter.messagebox.showinfo('提示', '请输入文件绝对路径')
+                return
+            if os.path.isfile(fileName):
+                if fileName[-4:] != 'json':
+                    tkinter.messagebox.showinfo('提示', '存在不是json的文件，请输入json文件')
+                fileNameList.append(fileName)
+            else:
+                tkinter.messagebox.showinfo('提示', '文件不存在')
         try:
-            processData.process(entry.get())
+            processData.process(fileNameList)
         except PermissionError:
-            tkinter.messagebox.showinfo('提示', '文件可能已打开或被占用，请先关闭')
+            tkinter.messagebox.showinfo('提示', '某些文件可能已打开或被占用，请先关闭')
+    return show
 
-    else:
-        tkinter.messagebox.showinfo('提示', '文件不存在')
+
+def clearFile():
+    lb.delete(0, lb.size())
 
 
 # 创建主窗口
@@ -38,27 +59,52 @@ wh = 300
 x = (sw - ww) / 2
 y = (sh - wh) / 2
 win.geometry("%dx%d+%d+%d" % (ww, wh, x, y))
+#-----------------------------------------------------------
+
+tab = ttk.Notebook(win)
+
+frame1 = tkinter.Frame(tab)
+tab1 = tab.add(frame1, text="单打习惯性出手线路")
+# 设置选中tab1
+tab.select(frame1)
+
+frame2 = tkinter.Frame(tab)
+tab2 = tab.add(frame2, text="双打得分情况统计")
+tab.pack(expand=True, fill=tkinter.BOTH)
+#-----------------------------------------------------------
 
 # 进入消息循环，可以写控件
-fm1 = tkinter.Frame(win)
-label = tkinter.Label(win,
-                      text="请输入文件绝对路径",
+fm1 = tkinter.Frame(frame1)
+fm2 = tkinter.Frame(frame1)
+label = tkinter.Label(frame1,
+                      text="请选择文件拖入（支持批量拖入）",
                       fg="red",
                       font=("黑体", 12),
-                      # width=20,
-                      # height=10,
-                      # wraplength=100,
                       justify="center",
                       anchor="ne")
-label.pack(side=TOP, pady=50)
+label.pack(side=TOP, pady=10)
 
-entry = tkinter.Entry(fm1, width=25, font=("Consolas", 15))
-entry.pack(side=LEFT)
-entry.focus()
-
-button = tkinter.Button(fm1, text="生成", command=showinfo)
-button.pack(side=RIGHT, padx=3)
+# sb = Scrollbar(fm1)    #垂直滚动条组件
+# sb.pack(side=BOTTOM, fill=X)  #设置垂直滚动条显示的位置
+lb = Listbox(fm1, height=7, width=50, selectmode=SINGLE)
+lb.pack(side=LEFT, fill=BOTH, expand=True)
+lb.focus()
+# sb.config(command=lb.xview)
 fm1.pack()
+
+windnd.hook_dropfiles(lb, func=dragged_files(lb))
+
+
+buttonGenerate = tkinter.Button(fm2, text="生成", command=showinfo(lb))
+buttonGenerate.pack(side=LEFT)
+buttonDelete = tkinter.Button(fm2, text="删除当前项", command=lambda item=lb: {
+    item.delete("active"),
+    tkinter.messagebox.showinfo('提示', '删除成功')
+})
+buttonDelete.pack(side=RIGHT, padx=10)
+buttonClear = tkinter.Button(fm2, text="清空", command=clearFile)
+buttonClear.pack(side=RIGHT, padx=10)
+fm2.pack(pady=10)
 
 win.mainloop()
 
---
+++ excelUtils.py
@@ -0,0 +1,61 @@
+from openpyxl.styles import Alignment
+
+# 生成列名字典，只是为了方便修改列宽时指定列，key:数字，从1开始；value:列名，从A开始
+def get_num_colnum_dict():
+    '''
+:return: 返回字典：{1:'A', 2:'B', ...... , 52:'AZ'}
+'''
+    num_str_dict = {}
+    A_Z = [chr(a) for a in range(ord('A'), ord('Z') + 1)]
+    AA_AZ = ['A' + chr(a) for a in range(ord('A'), ord('Z') + 1)]
+    A_AZ = A_Z + AA_AZ
+    for i in A_AZ:
+        num_str_dict[A_AZ.index(i) + 1] = i
+    return num_str_dict
+
+
+# 自适应列宽
+def style_excel(sheet):
+    '''
+    :param sheet_name:  excel中的sheet名
+    :return:
+    '''
+    # 选择对应的sheet
+    # 获取最大行数与最大列数
+    max_column = sheet.max_column
+    max_row = sheet.max_row
+
+    # 将每一列，单元格列宽最大的列宽值存到字典里，key:列的序号从1开始(与字典num_str_dic中的key对应)；value:列宽的值
+    max_column_dict = {}
+
+    # 生成列名字典，只是为了方便修改列宽时指定列，key:数字，从1开始；value:列名，从A开始
+    num_str_dict = get_num_colnum_dict()
+
+    # 遍历全部列
+    for i in range(1, max_column + 1):
+        # 遍历每一列的全部行
+        for j in range(1, max_row + 1):
+            #   为每个单元格添加居中样式
+            align = Alignment(horizontal='center', vertical='center')
+            sheet.cell(row=j, column=i).alignment = align
+            column = 0
+            # 获取j行i列的值
+            sheet_value = sheet.cell(row=j, column=i).value
+            # 通过列表生成式生成字符列表，将当前获取到的单元格的str值的每一个字符放在一个列表中（列表中一个元素是一个字符）
+            sheet_value_list = [k for k in str(sheet_value)]
+            # 遍历当前单元格的字符列表
+            for v in sheet_value_list:
+                # 判定长度，一个数字或一个字母，单元格列宽+=1.1，其它+=2.2（长度可根据需要自行修改，经测试一个字母的列宽长度大概为1）
+                if ord(v) <= 256:
+                    column += 1.1
+                else:
+                    column += 2.2
+            # 当前单元格列宽与字典中的对比，大于字典中的列宽值则将字典更新。如果字典没有这个key，抛出异常并将值添加到字典中
+            try:
+                if column > max_column_dict[i]:
+                    max_column_dict[i] = column
+            except Exception as e:
+                max_column_dict[i] = column
+    # 此时max_column_dict字典中已存有当前sheet的所有列的最大列宽值，直接遍历字典修改列宽
+    for key, value in max_column_dict.items():
+        sheet.column_dimensions[num_str_dict[key]].width = value + 1
